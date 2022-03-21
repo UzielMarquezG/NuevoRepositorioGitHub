10c92b207b798e0021bbf0e97ebf9ec44c142b4f
UzielMG <uzielmarquezgarcia@gmail.com>
2022-03-21 09:38:52 -0600

    Primera version original del codigo fuente

---
+++ PruebaGit/ClaseGIT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaGit
+{
+    class ClaseGIT
+    {
+
+        public static void Metodo1()
+        {
+            Console.WriteLine("Hola desde GIT en una nueva clase");
+
+        }
+
+    }
+
+}
---
+++ PruebaGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PruebaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola desde GIT");
+        }
+    }
+}
